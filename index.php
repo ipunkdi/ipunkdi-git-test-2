c0ffde164f8b20a3f03ca4e4911dac2923758bff
ipunkdi <dafaifaldi25@gmail.com>
2023-03-11 16:21:25 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas nemo molestiae error, tenetur, veritatis dicta est ullam illum inventore doloremque, ipsam praesentium nulla optio vel nihil. Ipsam quasi facilis eligendi!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
